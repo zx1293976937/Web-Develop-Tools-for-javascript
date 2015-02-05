5b3670576f48c0cca0e1efe5c10b80b354a7b397
Home_TP_WellerQu <xiaoyao.ning@gmail.com>
2015-02-05 15:10:26 +0800

    已解决模块化的问题
    但类与类之间的继承表现仍然需要调整

--- debug/milk.base-1.0.0.js
+++ debug/milk.base-1.0.0.js
@@ -1,5 +1,5 @@
 /*
- * core.js
+ * base.js
  * milk 框架的核心文件
  * 
  * v1.0.0
@@ -9,76 +9,52 @@
  
 (function() {
 	var moduleMap = {};
-	var javascriptFileMap = {};
+	// var javascriptFileMap = {};
 
 	var milk = {
-		define: (function(name, dependencies, factory) {
+		ObjectClass: (function() {
+			var self = this;
+
+			self.init = function() {
+				return self;
+			};
+
+			return self.init();
+		}),
+		define: (function(name, dependencies, descriptor) {
 			if (!moduleMap[name]) {
+				var dependencieDescriptors = [];
+				if (dependencies) {
+					for (var i = 0; i < dependencies.length; i++) {
+						var item = dependencies[i];
+
+						dependencieDescriptors.push(this.use(item));
+					};
+				};
+
 				var module = {
 					name: name,
-					dependencies: dependencies,
-					factory: factory
+					dependencieDescriptors: dependencieDescriptors,
+					descriptor: descriptor
 				};
 
 				moduleMap[name] = module;
 			};
 
-			return moduleMap[name];
+			return moduleMap[name];	// get a module's define
 		}), 
 		use: (function(name) {
 			var module = moduleMap[name];
 
 			if (!module) return null;
 
-			if(!module.entity) {
-				var args = [];
-				for (var i = 0; i < module.dependencies.length; i++) {
-					var dep = module.dependencies[i];
-					if (moduleMap[dep].entity)
-						args.push(moduleMap[dep].entity);
-					else
-						args.push(this.use(dep));
-				};
+			var dependencieDescriptors = module.dependencieDescriptors;
 
-				module.entity = module.factory.apply(null, args);
-			};
+			return new (function() {
+				var self = module.descriptor.apply(this, dependencieDescriptors);
 
-			return module.entity;
-		}),
-		requires: (function(files, callback) {
-			/*for (var i = 0; i < files.length; i++) {
-				var f = files[i];
-
-				if (!javascriptFileMap[f]) {
-					var headNode = document.getElementByTagName("head")[0];
-					var scriptNode = document.createElement("script");
-
-					scriptNode.type = "text/javascript";
-					scriptNode.async = "true";
-					scriptNode.src = name;
-					scriptNode.onload = function() {
-						javascriptFileMap[name] = true;
-						// headNode.removeChild(scriptNode);
-						checkAllFilesStatus();
-					};
-
-					headNode.appendChild(scriptNode);
-				};
-			};
-			
-			function checkAllFilesStatus() {
-				var allLoad = true;
-				for (var i = 0; i < files.length; i++) {
-					var f = files[i];
-					if (!javascriptFileMap[f]) {
-						allLoad = false;
-						break;
-					};
-				};
-
-				if (allLoad && callback)
-					callback();
-			};*/
+				return self;
+			});	
 		})
 	};
 
---
+++ debug/milk.storage-1.0.0.js
@@ -0,0 +1,59 @@
+/*
+ * storage.js
+ * milk 框架的本地存储管理器
+ * 
+ * v1.0.0
+ * created by Weller Qu
+ * 2015-02-04
+ */
+
+(function() {
+	if (!typeof window.milk) 
+		throw new Error("requires milk.base.js file");
+
+	window.milk.define("milk.storage.LocalDataManager", [], function() {
+		var self = window.milk.ObjectClass.call(this);
+
+		var storage = null; 
+
+		self.initWithStorage = function(storage) {
+			storage = storage;
+		};
+
+		self.setData = function(key, value) {
+			if (storage) {
+				storage.setItem(key, value);
+			};
+
+			return self;
+		};
+
+		self.getData = function(key) {
+			var t = null;
+
+			if (storage) {
+				t = storage.getItem(key);
+			};
+
+			return t;
+		};
+
+		self.removeData = function(key) {
+			if (storage) {
+				storage.removeItem(key);
+			};
+
+			return self;
+		};
+
+		self.clear = function() {
+			if (storage) {
+				for (var key in storage) {
+					storage.removeItem(key);
+				};
+			};
+
+			return self;
+		};
+	});
+})();
